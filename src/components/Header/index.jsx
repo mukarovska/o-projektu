ed5b17ea394f1ee31d250d28b0fab831610cd118
Karolína Mukařovská <k.mukarovska01@gmail.com>
2023-11-03 19:38:24 +0100

    menu

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+import { Menu } from '../Menu';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <Menu />
+    </header>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,10 @@
 import './style.css';
+import { Header } from '../../components/Header';
 
 export const HomePage = () => {
   return (
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <Header />
     </div>
   );
 };
